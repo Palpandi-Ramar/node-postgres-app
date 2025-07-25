55c69cb515288401200657ea8d3309111bebb02d
palpandi <palpandi.ramar@concertidc.com>
2025-07-25 15:21:47 +0530

    SSL fix for RDS connection issue

--- db.js
+++ db.js
@@ -6,7 +6,8 @@ const pool = new Pool({
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
-  port: 5432
+  port: 5432,
+  ssl: { require: true, rejectUnauthorized: false } // <-- Added SSL
 });
 
 module.exports = pool;
